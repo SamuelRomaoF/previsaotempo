596829cddb2553624cb31afe46fcf53ead7e7b71
Samuel <ofc.samuelromao@gmail.com>
2024-04-07 18:57:04 -0300

    primeiro

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
+const key = "d89ea649f45c02cb53ca6212d3be652e";
 
-
-const key = "d89ea649f45c02cb53ca6212d3be652e"
-
-
-async function buscarCidade(cidade){
-
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then( Resposta => resposta.json())
-
-    console.log(dados)
+function colocarDadosnatela(dados) {
+    console.log(dados);
+    document.querySelector(".cidade").textContent = "tempo em " + dados.name;
+    document.querySelector(".temp").textContent = Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".texto-previsao").textContent = dados.weather[0].description;
+    document.querySelector(".umidade").textContent = dados.main.humidity + "%";
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
 }
 
-
-function cliqueinoBotao() {
-    const input = document.querySelector(".input-cidade").value;
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt&units=metric`).then(resposta => resposta.json());
     
-    
-    
- 
-buscarCidade(input); // Corrigido para passar 'input' como argumento
+    colocarDadosnatela(dados);
 }
+
